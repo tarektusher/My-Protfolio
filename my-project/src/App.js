8352ea79c131241599f80498d523e2b77b84772b
tarektusher <mdtarek168504@gmail.com>
2023-12-01 02:11:41 -0600

    Nabbar is partially done

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Nabbar from './Components/Nabbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Nabbar></Nabbar>
+        <p>Hello TArek</p>
     </div>
   );
 }
