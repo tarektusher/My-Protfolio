f21fd764b40ffb55c6ae89387d8358db5d973bc9
tarektusher <mdtarek168504@gmail.com>
2023-12-05 14:47:37 +0600

    About page is created

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Nabbar from './Components/Nabbar';
+import AboutPage from './Pages/AboutPage';
 import HomePage from './Pages/HomePage';
 function App() {
   return (
     <div className="App">
         <Nabbar></Nabbar>
         <HomePage></HomePage>
+        <AboutPage></AboutPage>
     </div>
   );
 }
