8cbef1ce44c19bff46d55f45c8efe594c59cc53a
tarektusher <mdtarek168504@gmail.com>
2023-12-05 15:39:55 +0600

    About page img tag is not working now

--- my-project/src/Pages/AboutPage.jsx
+++ my-project/src/Pages/AboutPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
 function AboutPage() {
   return (
-    <div>AboutPage</div>
-  )
+    <div className="mt-10">
+      <h1 className="text-4xl text-mainColor">About me</h1>
+
+      <div className="md : flex justify-between items-center gap-12">
+        <div>
+          <img src="my pic.jpeg" alt="My Pic" />
+        </div>
+        <div>
+          <h1>Hi There! I'm Tarek Hossain Tusher</h1>
+          <h3>Software Engineer</h3>
+          <p>
+            I'm a Full Stack Developer. I Working on MERN AND PERN Stack. I'm
+            passionate about Competetive Programming. I solve more than 3000
+            problems in various judge.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        mainColor : "#24BFA7",
+        
+      }
+    },
   },
   plugins: [],
 }
